41f7dcdda91b6d74e64c7f641fb654cbb6246c74
Sergey Kudenko <sergeykudenko@Sergeys-MacBook-Air.local>
2024-11-15 01:46:41 +0100

    added string utils module

--- app/src/main/java/com/run/App.java
+++ app/src/main/java/com/run/App.java
@@ -1,12 +1,14 @@
 package com.run;
 
 import com.Constants;
+import org.apache.commons.lang3.StringUtils;
 import utils.PrintModule;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        PrintModule.print(Constants.MESSAGE);
+    public static void main( String[] args ) {
+        PrintModule.print(StringUtils.strip(Constants.MESSAGE));
+        PrintModule.print(StringUtils.strip("   1  TEST 1   "));
+
     }
 }
